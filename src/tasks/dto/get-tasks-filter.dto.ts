c1e554dabb52eb0443b7c8427049c61345eab0fc
jeounpar <cdex6531@ajou.ac.kr>
2022-07-28 01:39:54 +0900

    add decorator in filterdto

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,13 @@
+import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 import { TaskStatus } from '../tasks.model';
 
 export class GetTaskFilterDto {
+  @IsOptional()
+  @IsEnum(TaskStatus)
   status?: TaskStatus;
+
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
   search?: string;
 }
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -10,6 +10,7 @@ import {
 } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTaskFilterDto } from './dto/get-tasks-filter.dto';
+import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
 import { Task, TaskStatus } from './tasks.model';
 import { TasksService } from './tasks.service';
 
@@ -42,7 +43,11 @@ export class TasksController {
   }
 
   @Patch('/:id/status')
-  patchTaskStatus(@Param('id') id: string, @Body('status') status: TaskStatus) {
+  patchTaskStatus(
+    @Param('id') id: string,
+    @Body() updateTasksStatusDto: UpdateTaskStatusDto,
+  ): Task {
+    const { status } = updateTasksStatusDto;
     return this.taskService.patchTaskStatus(id, status);
   }
 }
